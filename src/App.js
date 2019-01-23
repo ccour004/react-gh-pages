b475eaa73a28aed6de59e0e7621948cc2b8f50a7
Colin Courtney <ccourtney@mitre.org>
2019-01-23 09:17:33 -0500

    Testing small change.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
+<img src={logo} className="App-logo" alt="logo" />
             Learn React
           </a>
         </header>
